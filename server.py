6ca5943b16dc97d49a02a6bf171765c7a223e354
baasi <basijr10@gmail.com>
2019-05-01 20:53:54 +0200

    finished

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import http.server
 import socketserver
-import json
 import http.client
 import requests
 
@@ -10,9 +9,9 @@ SERVER = "https://rest.ensembl.org"
 ENDPOINT = ["/info/species", '/info/assembly']
 EPORT = 80
 
-TEST_REPORT = False #If you wanna see
 
 socketserver.TCPServer.allow_reuse_address = True
+
 
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
@@ -22,12 +21,6 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         print("Request line:" + self.requestline)
         print("  Cmd: " + self.command)
         print("  Path: " + self.path)
-
-        writing_report_test("--------------NEW REQUEST----------------")
-        writing_report_test("Request line:" + self.requestline)
-        writing_report_test("  Cmd: " + self.command)
-        writing_report_test("  Path: " + self.path)
-
 
         contents = ""
         try:
@@ -39,22 +32,18 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                     self.send_response(200)
                     self.send_header('Content-Type', 'text/html')
 
-
             else:
                 end = self.path.split("?")[0]
-                writing_report_test("  Endpoint: " + end)
                 print ("End =>", end)
                 if end == '/listSpecies':
                         contents = self.handle_info_species()
                         self.send_response(200)
                         self.send_header('Content-Type', 'text/html')
 
-
                 elif end == '/karyotype':
                     contents = self.handle_info_assembly()
                     self.send_response(200)
                     self.send_header('Content-Type', 'text/html')
-
 
                 elif end == '/chromosomeLength':
                     contents = self.handle_length()
@@ -78,20 +67,18 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-Type', 'text/html')
 
-
         self.send_header("Content-Length", len(str.encode(str(contents))))
+
         self.end_headers()
 
         self.wfile.write(str.encode(contents))
 
         return
 
-
     def handle_info_species(self):
         request = SERVER + ENDPOINT[0]
         r = requests.get(request, headers=headers)
         print("Sending request:", request)
-        writing_report_test("request: " + request)
         d = r.json()
 
         contents = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Species List</title></head>' \
@@ -110,7 +97,7 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
 
             contents += "</ol></body></html>"
         else:
-            for index in range(len(d['species'][:int(l)])):
+            for index in range(len(d['species'][:int(limit)])):
                 contents += "<li>"
                 contents += d['species'][index]['common_name']
                 contents += "</li>"
@@ -119,19 +106,16 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
 
         return contents
 
-
     def handle_info_assembly(self):
         specie = self.path.split("=")[1]
         specie = specie.replace("+", "_")
         request = SERVER + ENDPOINT[1] + "/" + specie
         print ("Sending request:", request)
-        writing_report_test("request: " + request)
 
         r = requests.get(request, headers=headers)
         d = r.json()
         print("CONTENT: ")
         print(d)
-
 
         contents = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Karyotype of ' + specie + '</title></head>' \
                    '<body style="background-color: turquoise;"><h1>Karyotype of ' + specie + '</h1><ol>'
@@ -145,8 +129,6 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
 
         return contents
 
-
-
     def handle_length(self):
         print("\nConnecting to server: {}:{}\n".format(SERVER, EPORT))
         specie = self.path.split("=")[1].split("&")[0]
@@ -156,14 +138,11 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         chromo = self.path.split("&")[1].split("=")[1]
         print("chromo='",chromo, "', specie = '", specie, "'", sep="")
 
-        # Send the request message
         request = SERVER + ENDPOINT[1] + "/" + specie
         print(request)
-        writing_report_test("request: " + request)
         r = requests.get(request, headers=headers)
 
         d = r.json()
-
 
         length_chromosome = None
         for element in d["top_level_region"]:
@@ -187,14 +166,7 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                    '<body><h1>The length of the chromosome ' + chromo + ' of the specie ' + specie + 'is ' + str(length_chromosome) + '.</h1>'
             contents += '</body></html>'
 
-
         return contents
-
-def writing_report_test(info):
-    if TEST_REPORT:
-        f = open("test_report.txt", "a")
-        f.write(info + "\n")
-        f.close()
 
 
 Handler = TestHandler
